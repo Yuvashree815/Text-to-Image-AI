066a122f0cf46e3b78c2f5d3e34b5704983358d9
Yuvashree815 <143113191+Yuvashree815@users.noreply.github.com>
2024-08-22 23:18:26 +0530

    image.py

--- image.py
+++ image.py
@@ -1,19 +1,27 @@
 import requests
 import streamlit as st
+import io
+from PIL import Image
 
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/sdxl-turbo"
 headers = {"Authorization": "Bearer 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"}
 
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.content
-image_bytes = query({
-	"inputs": st.text_input('Enter a prompt'),
-})
-# You can access the image with PIL.Image for example
-import io
-from PIL import Image
-image = Image.open(io.BytesIO(image_bytes))
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.content
+
+prompt = st.text_input('Enter a prompt')
 
 if st.button('Generate'):
-	st.image(image)
+    if prompt:
+        image_bytes = query({"inputs": prompt})
+        
+        # Check if the response is an image
+        try:
+            image = Image.open(io.BytesIO(image_bytes))
+            st.image(image, caption='Generated Image')
+        except Exception as e:
+            st.error("Failed to generate image. Please try again.")
+            st.write(e)
+    else:
+        st.error("Please enter a prompt.")
